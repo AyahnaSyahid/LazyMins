f3190a87ddabe9d78cba36ac522a1ccb321b1603
A3PLUS <ayah.syahid2017@gmail.com>
2025-05-02 19:57:01 +0700

    Mencoba membuat kelas manager generic

--- src/database/customersmanager.cpp
+++ src/database/customersmanager.cpp
@@ -1,17 +1,21 @@
 #include "customersmanager.h"
+#include <QSqlQuery>
+#include <QSqlError>
+
 
 CustomersManager::CustomersManager(QObject* parent) : QObject(parent) {}
 CustomersManager::~CustomersManager(){}
 
 
-bool CustomersManager::add(const QVariantMap&) {
+bool CustomersManager::add(const QVariantMap& v) {
+    QSqlQuery q;
     
 }
 
-bool CustomersManager::erase(const QVariantMap&) {
+bool CustomersManager::erase(const QVariantMap& v) {
     
 }
 
-bool CustomersManager::update(const QVariantMap&, const QVariantMap&) {
+bool CustomersManager::update(const QVariantMap& o, const QVariantMap& n) {
     
 }
--- src/database/tablemanager.h
+++ src/database/tablemanager.h
@@ -1,27 +1,32 @@
 #ifndef TableManager_H
-    #define TableManager_H
+#define TableManager_H
 
 #include <QObject>
 #include <QVariant>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlRecord>
 
 class TableManager : public QObject {
     Q_OBJECT
-    QString tableName;
-
+    QString _tableName;
+    QSqlError _lastError();
 public:
     TableManager(const QString& tableName, QObject* = nullptr);
     ~TableManager();
     QSqlRecord record() const;
+    const QSqlError& lastError() const { return _lastError; }
+    const QString& tableName() const { return _tableName; }
 
 public slots:
-    virtual bool insert(const QVariantMap&);
+    virtual bool add(const QVariantMap&);
     virtual bool erase(const QVariantMap&);
     virtual bool update(const QVariantMap&, const QVariantMap&);
 
 signals:
-    void inserted(const QVariantMap&);
-    void erased(const QVariantMap&);
-    void updated(const QVariantMap&);
+    void inserted(const QVariant lastId);
+    void erased(const QVariant erasedId);
+    void updated(const QVariant updatedId);
 };
 
 #endif

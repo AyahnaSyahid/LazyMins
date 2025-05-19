e182ff461a6f5a7d401be75f706aaef4565ee1ff
A3PLUS <ayah.syahid2017@gmail.com>
2025-05-19 11:41:19 +0700

    CreateInvoiceDialog Class Should be OK now

--- src/ui/createinvoicedialog.cpp
+++ src/ui/createinvoicedialog.cpp
@@ -109,16 +109,10 @@ bool CreateInvoiceDialog::saveInvoice(QSqlError* ref) {
     qDebug() << "WHERE STMT : " << where;
     
     QSqlQuery q("BEGIN;");
-    q.prepare("SELECT COALESCE(MAX(CAST(invoice_enum AS INTEGER)), 0) + 1 FROM invoices WHERE invoice_date = ?");
-    q.addBindValue(ui->invoiceDate->date().toString("yyyy-MM-dd"));
-    q.exec() && q.next();
-
-    int invEnum = q.value(0).toInt();
-
-    q.prepare("INSERT INTO invoices (invoice_enum, invoice_date, customer_id, user_id, total_amount, discount, notes) "
-        "VALUES (?, ?, ?, ?, ?, ?, ?);");
+    q.prepare("INSERT INTO invoices (daily_enum, invoice_date, customer_id, user_id, total_amount, discount, notes)"
+        "VALUES ((SELECT COALESCE(MAX(daily_enum), 0) + 1 FROM invoices WHERE invoice_date = ?), ?, ?, ?, ?, ?, ?);");
     
-    q.addBindValue(QString::number(invEnum));
+    q.addBindValue(ui->invoiceDate->date().toString("yyyy-MM-dd"));
     q.addBindValue(ui->invoiceDate->date().toString("yyyy-MM-dd"));
     q.addBindValue(custId);
     q.addBindValue(uman->currentUser());
@@ -132,9 +126,8 @@ bool CreateInvoiceDialog::saveInvoice(QSqlError* ref) {
         return false;
     }
     savedInvoiceId = q.lastInsertId().toInt();
-    QString upd("UPDATE orders SET invoice_id = ? WHERE %1");
+    QString upd("UPDATE orders SET invoice_id = last_insert_rowid() WHERE %1");
     q.prepare(upd.arg(where));
-    q.addBindValue(savedInvoiceId);
     if(!q.exec()) {
         QMessageBox::information(this, "Gagal", QString("Tidak dapat mengupdate orders\nError:\n%1").arg(q.lastError().text()));
         q.exec("ROLLBACK");
@@ -150,19 +143,16 @@ bool CreateInvoiceDialog::saveInvoice(QSqlError* ref) {
 
 void CreateInvoiceDialog::on_saveButton_clicked() {
     QSqlError err;
-    if(!saveInvoice(&err)) {
-        if(err.isValid()) {
-            QMessageBox::information(this, "Gagal menyimpan", QString("Error :").arg(err.text()));
-            return ;
-        }
-        QMessageBox::information(this, "Gagal menyimpan", "Error :\nTidak diketahui");
-        return ;
+    if(saveInvoice(&err)) {
+        accept();
     }
-    accept();
 }
 
 void CreateInvoiceDialog::on_payButton_clicked() {
-    on_saveButton_clicked();
+    QSqlError err;
+    if(!saveInvoice(&err)) {
+        return;
+    }
     if(savedInvoiceId > -1) {
         emit openPayment(savedInvoiceId);
     }

1574f6be90c0e93a6ac89fb0902ac873b391d9ec
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-02-14 07:04:32 +0700

    Beuh Lieur

--- src/widget/receiptpreviewdialog.cpp
+++ src/widget/receiptpreviewdialog.cpp
@@ -8,49 +8,60 @@
 #include <QPushButton>
 #include <QFont>
 #include <QPen>
-#include <QGraphicsLineItem>
+#include <QGraphicsSimpleTextItem>
 #include <QLocale>
 #include <QRectF>
 
 namespace {
-  QMap<QString, QFont> m_font;
-  
-  void initFont() {
-    static bool initialized;
-    if (!initialized) {
-      m_font["normal"] = QFont("Consolas", 9);
-      m_font["bold"] = QFont("Consolas", 9);
-      m_font["bold"].setBold(true);
-      m_font["big"] = QFont("Consolas", 14);
-      m_font["big"].setBold(true);
-      initialized = true;
-    }
-  }
-  
   PrintInvoiceParams getParam(qlonglong inv) {
     return DatabaseInterface::instance().getPrintInvoiceParams(inv);
   }
 
-  QRectF drawHeader(QGraphicsScene *scn, const StoreInfo &si) {
-    qreal y;
-    QRecF g1rect;
-    auto &font = m_font;
-    auto g1 = scn->addSimpleText("================================", font["normal"]);
-    g1rect = g1->boundingRect();
-    g1 = scn->addSimpleText(si.storeName, font("big"));
-    auto r1 = g1.boundingRect();
-    r1.moveCenter(g1rect.center());
-    r1.top(g1rect.bottom());
-    g1->setPos(r1.topLeft());
-    g1 = scn->addSimpleText(si.storeAddr, font["normal"]);
+  QString wrapText(const QString &text, int maxChars) {
+    if (maxChars <= 0) return text;
+
+    // Split berdasarkan spasi DAN newline untuk mendapatkan semua kata
+    // Qt::SkipEmptyParts akan otomatis membuang spasi ganda dan newline kosong
+    QStringList words = text.split(QRegExp("[\s\n]+"), Qt::SkipEmptyParts);
+
+    QStringList result;
+    QString currentLine;
+
+    for (QString word : words) {
+      // Kasus 1: Kata sangat panjang (Potong Paksa)
+      if (word.length() > maxChars) {
+        if (!currentLine.isEmpty()) {
+            result.append(currentLine);
+            currentLine.clear();
+        }
+        
+        while (word.length() > maxChars) {
+            result.append(word.left(maxChars));
+            word = word.mid(maxChars);
+        }
+        currentLine = word; 
+      } 
+      // Kasus 2: Kata muat di baris saat ini
+      else if (currentLine.isEmpty() || (currentLine.length() + 1 + word.length() <= maxChars)) {
+        if (!currentLine.isEmpty()) currentLine += " ";
+        currentLine += word;
+      } 
+      // Kasus 3: Baris penuh, buat baris baru
+      else {
+        result.append(currentLine);
+        currentLine = word;
+      }
+    }
+
+    if (!currentLine.isEmpty()) result.append(currentLine);
+    return result.join("\n");
+  }
+  
+  void drawSingleItem(QGraphicsScene *s, const InvoiceData:ItemData &i, const QPointF &pos, const QFont &f) {
+    auto gName = s->addSimpleText(wrapText(i.productName, 32), f);
+    gName->setPos(pos)
+    auto r1 = gName->boundingRect();
     
-    g1->setY(r1.bottom());
-    r1 = g1.boundingRect();
-    g1 = scn->addSimpleText(si.storePhone, font["normal"]);
-    g1->setY(r1.bottom());
-    r1 = g1.boundingRect();
-    g1 = scn->addSimpleText("================================", font["normal"]);
-    g1->setY(r1.bottom());
   }
 }
 
@@ -61,7 +72,6 @@ ReceiptPreviewDialog::ReceiptPreviewDialog(qlonglong inv, QWidget *parent)
   ui->rView->setScene(scene);
   auto &di = DatabaseInterface::instance();
   m_ready = !param.adminName.isEmpty();
-  initFont();
   draw();
 }
 
@@ -71,10 +81,14 @@ void ReceiptPreviewDialog::draw()
 {
   scene->clear();
   if (!m_ready) return ;
+  QFont normalFont("Courier New", 9),
+        boldFont("Courier New", 9),
+        bigFont("Courier New", 14);
+  QString eqLine(32, QChar('=')), dashLine(32, QChar('-'));
   boldFont.setBold(true);
   bigFont.setBold(true);
   QRectF sr(1,1,1,1);
-  auto g1 = scene->addSimpleText("================================", normalFont);
+  auto g1 = scene->addSimpleText(eqLine, normalFont);
   auto g2 = scene->addSimpleText(param.storeInfo.storeName, bigFont);
   g2->setY(g1->boundingRect().bottomLeft().y());
   auto r1 = g1->boundingRect();
@@ -94,21 +108,21 @@ void ReceiptPreviewDialog::draw()
   r2.moveCenter(r1.center());
   r2.moveTop(r1.bottom());
   g2->setPos(r2.topLeft());
-  g1 = scene->addSimpleText("================================", normalFont);
+  g1 = scene->addSimpleText(eqLine, normalFont);
   r1 = g1->boundingRect();
   r1.moveCenter(r2.center());
   r1.moveTop(r2.bottom());
   g1->setPos(r1.topLeft());
-  g2 = scene->addSimpleText("Nama Barang", normalFont);
+  g2 = scene->addSimpleText("   Nama Barang", normalFont);
   r2 = g2->boundingRect();
   r2.setTopLeft(r1.bottomLeft());
   g2->setPos(r2.topLeft());
-  g1 = scene->addSimpleText("Harga", normalFont);
+  g1 = scene->addSimpleText("Harga    ", normalFont);
   r1 = g1->boundingRect();
   r1.moveTop(r2.top());
   r1.moveRight(sr.right());
   g1->setPos(r1.topLeft());
-  g1 = scene->addSimpleText("-----------------------------------", normalFont);
-  g1->setPos(r2.bottomLeft());
-  drawInvoiceItems()
+  r2 = g1->boundingRect();
+  g1 = scene->addSimpleText(dashLine, normalFont);
+  g1->setY(r1.bottom());
 }

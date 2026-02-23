9131ccfea32c9c38817d14b17246791f16dd09e7
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-02-23 07:23:06 +0700

    removed src/models

--- src/models/konsumenmanager.cpp
+++
@@ -1,8 +0,0 @@
-#include "konsumenmanager.h"
-
-QSqlRecord KonsumenManager::create(const QVariantMap& map) {
-  if(!map.contains("nama_lengkap")) {
-    return QSqlRecord();
-  }
-  return BaseManager::create(map);
-}
--- src/models/productcategoriesmanager.cpp
+++
@@ -1,5 +0,0 @@
-#include "productcategoriesmanager.h"
-
-bool ProductCategoriesManager::setState(int id, int state) {
-  return update(id, {{"is_active", state ? true : false}});
-}

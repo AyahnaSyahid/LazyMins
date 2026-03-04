49558d9fa4fca6476882bd355e5dc5430ff6effe
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-03-04 15:45:03 +0700

    added calculating step

--- src/dialogs/orderitemdialog.cpp
+++ src/dialogs/orderitemdialog.cpp
@@ -1,5 +1,6 @@
 #include "orderitemdialog.h"
 #include "ui_orderitemdialog.h"
+#include <QMessageBox>
 
 OrderItemDialog::OrderItemDialog(QWidget *parent) :
     FormDialog(parent),
@@ -7,7 +8,9 @@ OrderItemDialog::OrderItemDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     setupFields();
-    ui->produkComboBox->setQuery("SELECT id, name, description FROM products");
+    ui->produkComboBox->setQuery("SELECT id, name, description, use_area, cost_price FROM products");
+    ui->produkComboBox->showColumn(3, false); // Sembunyikan kolom use_area
+    ui->produkComboBox->showColumn(4, false); // Sembunyikan kolom cost_price
     ui->produkComboBox->boxViewAutoResize();
     ui->produkComboBox->setCurrentIndex(-1);
 }
@@ -25,12 +28,14 @@ bool OrderItemDialog::onSave(const QVariantMap& changes) {
 void OrderItemDialog::setupFields() {
     setFields({
         { ui->namaLineEdit, "product_name" },
-        { ui->hargaSpinBox, "base_price" },
+        { ui->hargaSpinBox, "sale_price" },
         { ui->qtySpinBox, "quantity" },
         { ui->subtotalSpinBox, "subtotal" },
         { ui->diskonDoubleSpinBox, "discount_percentage" },
         { ui->diskonRpSpinBox, "discount_amount" },
-        { ui->notesTextEdit, "notes" }
+        { ui->notesTextEdit, "notes" },
+        { ui->widthBox, "size_width" },
+        { ui->heightBox, "size_height" }
     });
 }
 
@@ -43,4 +48,103 @@ void OrderItemDialog::setupBoundFields() {
                 int index = ui->produkComboBox->findValue(id);
                 ui->produkComboBox->setCurrentIndex(index);
             });
+}
+
+void OrderItemDialog::on_simpanButton_clicked()
+{
+    // validasi ui->produkComboBox harus >= 0
+    if (ui->produkComboBox->currentIndex() < 0) {
+        QMessageBox::warning(this, "Validasi", "Produk harus dipilih");
+        return;
+    }
+    if (m_autoCommit) {
+        accept();
+        return;
+    }
+    hide();
+}
+
+void OrderItemDialog::on_produkComboBox_currentIndexChanged(int index) {
+    if (index < 0) {
+        ui->hargaSpinBox->setMinimum(0);
+        ui->hargaSpinBox->setValue(0);
+        return;
+    }
+    auto model = ui->produkComboBox->model();
+    auto use_area = model->index(index, 3).data(Qt::EditRole);
+    if (use_area.toInt() == 1) { // jika produk menggunakan area, aktifkan input width & height
+        ui->widthBox->setEnabled(true);
+        ui->heightBox->setEnabled(true);
+    } else {
+        ui->widthBox->setEnabled(false);
+        ui->heightBox->setEnabled(false);
+    }
+    // dapatkan current product id
+    int productId = model->index(index, 0).data(Qt::EditRole).toInt();
+    // dapatkan harga berdasarkan price level customer
+    auto optprice = m_priceManager.getPrice(productId, m_customerPriceLevel);
+    if (optprice.has_value()) {
+        ui->hargaSpinBox->setMinimum(*optprice);
+        ui->hargaSpinBox->setValue(*optprice);
+    } else {
+        // fallback ke harga dasar dari tabel products
+        int basePrice = model->index(index, 4).data(Qt::EditRole).toInt();
+        ui->hargaSpinBox->setMinimum(basePrice);
+        ui->hargaSpinBox->setValue(basePrice);
+    }
+}
+
+void OrderItemDialog::on_hargaSpinBox_valueChanged(int arg1)
+{
+    Q_UNUSED(arg1);
+    recalculateSubtotal();
+}
+
+void OrderItemDialog::on_qtySpinBox_valueChanged(int arg1)
+{
+    Q_UNUSED(arg1);
+    recalculateSubtotal();
+}
+
+void OrderItemDialog::on_diskonDoubleSpinBox_valueChanged(double arg1)
+{
+    double calcullatedSubtotal = calculatedPrice();
+    double diskonRp = calcullatedSubtotal * (arg1 / 100.0);
+    // diskonRp harus dibudate benggunakan metode roundUp agar tidak terjadi pembulatan ke bawah yang merugikan pelanggan
+    diskonRp = std::ceil(diskonRp);
+    ui->diskonRpSpinBox->setValue(diskonRp);
+}
+
+void OrderItemDialog::on_diskonRpSpinBox_valueChanged(int arg1)
+{
+    Q_UNUSED(arg1);
+    recalculateSubtotal();
+}
+
+void OrderItemDialog::recalculateSubtotal()
+{
+    // diskonPersen dan diskonRp adalah entitas yang sama, 
+    // jika user mengatur diskonPersen maka diskonRp harus dihitung ulang, begitu pula sebaliknya. 
+    // Untuk menyederhanakan, kita asumsikan user hanya akan mengatur salah satu jenis diskon, dan kita prioritaskan diskonRp jika keduanya diisi.
+    // double diskonPersen = ui->diskonDoubleSpinBox->value();
+    double subtotal = calculatedPrice();
+    ui->diskonRpSpinBox->setMaximum(subtotal);
+    double diskonRp = ui->diskonRpSpinBox->value();
+    subtotal -= diskonRp; // Diskon nominal
+    ui->subtotalSpinBox->setValue(subtotal);
+}
+
+double OrderItemDialog::calculatedPrice() const
+{
+    if (ui->produkComboBox->currentIndex() < 0) {
+        return 0.0;
+    }
+    auto model = ui->produkComboBox->model();
+    int qty = ui->qtySpinBox->value();
+    double width = ui->widthBox->value();
+    double height = ui->heightBox->value();
+    double harga = ui->hargaSpinBox->value();
+    auto use_area = model->index(ui->produkComboBox->currentIndex(), 3).data(Qt::EditRole);
+    double areaMultiplier = (width > 0 && height > 0) ? (width * height) : 1.0;
+    return harga * areaMultiplier * qty;
 }
--- src/dialogs/orderitemdialog.h
+++ src/dialogs/orderitemdialog.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "formdialog.h"
-
+#include "src/managers/managers.h"
 namespace Ui {
     class OrderItemDialog;
 }
@@ -17,8 +17,34 @@ public:
     void setupFields() override;
     void setupBoundFields() override;
 
+    QVariantMap getFieldData() const {
+        return collect();
+    }
+    
+    void setAutoCommit(bool autoCommit) {
+        m_autoCommit = autoCommit;
+    }
+    
+    void setCustomerPriceLevel(int priceLevel) {
+        // kita handle ini di implementasi on_produkComboBox_currentIndexChanged, jadi simpan saja di member variable
+        m_customerPriceLevel = priceLevel;
+    }
+
 private slots:
-    void on_simpanButton_clicked() { onSave(collect()); }
+    void on_produkComboBox_currentIndexChanged(int index);
+    void on_simpanButton_clicked();
+
+    void on_hargaSpinBox_valueChanged(int arg1);
+    void on_qtySpinBox_valueChanged(int arg1);
+    void on_diskonDoubleSpinBox_valueChanged(double arg1);
+    void on_diskonRpSpinBox_valueChanged(int arg1);
+    void on_widthBox_valueChanged(double arg1) { recalculateSubtotal(); }
+    void on_heightBox_valueChanged(double arg1) { recalculateSubtotal(); }
+    void recalculateSubtotal();
 private:
+    double calculatedPrice() const;
     Ui::OrderItemDialog *ui;
+    bool m_autoCommit = true;
+    int m_customerPriceLevel = 1;
+    ProductPriceManager m_priceManager;
 };

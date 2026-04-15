0c530bea9f9862f08174466ce810e769d43e8783
AyahnaBarudak <42292094+AyahnaSyahid@users.noreply.github.com>
2026-04-15 22:02:03 +0700

    Add POS printer management header file

---
+++ src/utils/posprinter.h
@@ -0,0 +1,10 @@
+// POS Printer Management Header
+// Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2026-04-15 15:00:35
+// Current User's Login: AyahnaSyahid
+
+#ifndef POS_PRINTER_H
+#define POS_PRINTER_H
+
+// Function prototypes and definitions will go here
+
+#endif // POS_PRINTER_H

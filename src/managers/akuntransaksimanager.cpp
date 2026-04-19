f91105afa2d199ba7fe9fddd01f64416ff76733a
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-04-19 18:28:55 +0700

    Add: Deposit Dialog

--- src/managers/akuntransaksimanager.cpp
+++ src/managers/akuntransaksimanager.cpp
@@ -43,6 +43,12 @@ bool AkunTransaksiManager::opname(int id, int newSaldo, const QString& deskripsi
     int currentUserId = SessionManager::instance().currentUser()->value("id").toInt();
 
     SqlTransaction t;
+
+    if(!t.started()) {
+        setErrorString("Tidak dapat memulai transaksi database");
+        return false;
+    }
+
     auto q = baseQuery();
     
     q.prepare("SELECT saldo FROM akun_transaksi WHERE id = :id");
@@ -88,6 +94,63 @@ bool AkunTransaksiManager::opname(int id, int newSaldo, const QString& deskripsi
         {"amount_before", oldSaldo},
         {"amount", newSaldo - oldSaldo},
         {"amount_after", newSaldo},
+        {"created_at", QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd HH:mm:ss")},
+        {"updated_at", QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd HH:mm:ss")}
+    });
+    if(!opt_tr.has_value()) {
+        setErrorString(trm.errorString());
+        return false;
+    }
+    return t.commit();
+}
+
+bool AkunTransaksiManager::deposit(int id, int nominal, const QString &deskripsi)
+{
+    if (!SessionManager::instance().currentUser().has_value()){
+        setErrorString("Tidak ada user aktif");
+        return false;
+    }
+    int currentUserId = SessionManager::instance().currentUser()->value("id").toInt();
+    
+    SqlTransaction t;
+    if(!t.started()) {
+        setErrorString("Tidak dapat memulai transaksi database");
+        return false;   
+    }
+
+    auto q = baseQuery();
+    q.prepare("SELECT saldo FROM akun_transaksi WHERE id = :id");
+    q.bindValue(":id", id);
+    if (!q.exec() || !q.next()) {
+        setErrorString(q.lastError().text());
+        return false;
+    }
+
+    int saldo = q.value("saldo").toInt();
+
+    if (saldo + nominal < 0) {
+        setErrorString("Saldo tidak mencukupi");
+        return false;
+    }
+
+    q.prepare("UPDATE akun_transaksi SET saldo = saldo + :nominal WHERE id = :id");
+    q.bindValue(":nominal", nominal);
+    q.bindValue(":id", id);
+    if (!q.exec()) {
+        setErrorString(q.lastError().text());
+        return false;
+    }
+
+    TransaksiManager trm;
+    auto opt_tr = trm.create({
+        {"akun_id", id},
+        {"tipe", nominal > 0 ? "pemasukan" : "pengeluaran"},
+        {"transaction_number", TransaksiManager::nextNumber()},
+        {"admin_id", currentUserId},
+        {"deskripsi", deskripsi},
+        {"amount_before", saldo},
+        {"amount", nominal},
+        {"amount_after", saldo + nominal},
         {"created_at", QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd HH:mm:ss")},
         {"updated_at", QDateTime::currentDateTimeUtc().toString("yyyy-MM-dd HH:mm:ss")}
     });

1655df6d0ec1ec50eb0550f9b8ed8fb25d526460
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-04-19 10:27:21 +0700

    Fix: Kalkulasi Ui

--- src/dialogs/akuntransaksiopnamedialog.cpp
+++ src/dialogs/akuntransaksiopnamedialog.cpp
@@ -1,10 +1,11 @@
-#include "akuntransakiopnamedialog.h"
+#include "akuntransaksiopnamedialog.h"
 #include "ui_akuntransaksiopnamedialog.h"
 
 #include "src/managers/akuntransaksimanager.h"
+#include <QMessageBox>
 
 AkunTransaksiOpnameDialog::AkunTransaksiOpnameDialog(QWidget *parent)
-    : QDialog(parent) , ui(new Ui::AkunTransasiOpnameDialog)
+    : QDialog(parent) , ui(new Ui::AkunTransaksiOpnameDialog)
 {
     ui->setupUi(this);
 }
@@ -24,18 +25,27 @@ bool AkunTransaksiOpnameDialog::prepareOpname(int akunId)
     ui->nameLabel->setText(m_record.value("name").toString());
     ui->currentBox->setValue(m_record.value("saldo").toInt());
     ui->realBox->setValue(ui->currentBox->value());
-    ui->adjustBox->setValue(ui->currentBox->value() - ui->realBox->value());
+    ui->adjustBox->setValue(ui->realBox->value() - ui->currentBox->value());
     return true;
 }
 
 void AkunTransaksiOpnameDialog::on_realBox_valueChanged(int a) {
-    ui->adjustBox->setValue(ui->currentBox->value() - a);
+    ui->adjustBox->setValue(a - ui->currentBox->value());
 }
 
 void AkunTransaksiOpnameDialog::on_simpanButton_clicked() {
-    if (ui->notesEdit->toPlainText().simplified().isEmpty()) {
-        QMessageBox::warning(this, "Berikan Catatan", "Opname / Adjustment harus disertai dengan alasannya");
-        return ;
+    if (ui->adjustBox->value() == 0 ) {
+      accept();
+    } else { 
+      if (ui->notesEdit->toPlainText().simplified().isEmpty()) {
+          QMessageBox::warning(this, "Berikan Catatan", "Opname / Adjustment harus disertai dengan alasannya");
+          return ;
+      }
+      AkunTransaksiManager mgr;
+      if(!mgr.opname(m_record.value("id").toInt(), ui->realBox->value(), ui->notesEdit->toPlainText())) {
+          QMessageBox::warning(this, "Gagal melakukan Opname", mgr.errorString());
+          return ;
+      }
+      accept();
     }
-    
 }

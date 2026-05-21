04366588a49ec1abfbefea199c4f61e34be5483c
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-05-21 15:48:27 +0700

    Fix: Filtering

--- src/display/invoicebrowser.cpp
+++ src/display/invoicebrowser.cpp
@@ -59,7 +59,7 @@ SELECT inv.id,
   model().setHeaderData(21, Qt::Horizontal, "I Notes", Qt::DisplayRole);
 
   setColumnVisible(0, false);
-
+  setFilterColumnNames({"invoice_number", "customer_name"});
   Ui()->dataView->setContextMenuPolicy(Qt::CustomContextMenu);
   connect(Ui()->dataView, &QAbstractItemView::customContextMenuRequested, this,
           &InvoiceBrowser::on_dataView_customContextMenuRequested);

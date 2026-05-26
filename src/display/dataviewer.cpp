57a0d7644667bebab6422adbc21a816d8caf16bf
AyahnaSyahid <ayah.syahid2017@gmail.com>
2026-05-26 15:53:50 +0700

    Refresh Shortcut for every DataViewer subclass

--- src/display/dataviewer.cpp
+++ src/display/dataviewer.cpp
@@ -1,112 +1,126 @@
 #include "dataviewer.h"
 #include "ui_dataviewer.h"
 
-#include <QPushButton>
-#include <QTimer>
-#include "src/managers/managers.h"
 #include "src/customs/booleandelegate.h"
+#include "src/managers/managers.h"
+#include <QKeySequence>
+#include <QPushButton>
+#include <QShortcut>
+#include <QTimer>
 
-DataViewer::DataViewer(QWidget *parent) : ui(new Ui::DataViewer), 
-  m_model(this), m_filterTimer(this), m_columns {}, 
-  m_filterColumnNames {}, QWidget(parent)
+DataViewer::DataViewer(QWidget *parent) : ui(new Ui::DataViewer),
+                                          m_model(this), m_filterTimer(this), m_columns{},
+                                          m_filterColumnNames{}, QWidget(parent)
 {
-    ui->setupUi(this);
-    ui->dataView->setModel(&m_model);
-    connect(&m_model, &QAbstractItemModel::rowsInserted, this, &DataViewer::updateNavigation);
-    connect(&m_model, &QAbstractItemModel::rowsRemoved, this, &DataViewer::updateNavigation);
-    connect(ui->firstButton, &QPushButton::clicked, [=]
-            {m_model.setPage(1); refresh(); });
-    connect(ui->nextButton, &QPushButton::clicked, [=]
-            {m_model.setPage(m_model.currentPage() + 1); refresh(); });
-    connect(ui->prevButton, &QPushButton::clicked, [=]
-            {m_model.setPage(m_model.currentPage() - 1); refresh(); });
-    connect(ui->lastButton, &QPushButton::clicked, [=]
-            {m_model.setPage(m_model.totalPages()); refresh(); });
-    m_filterTimer.setSingleShot(true);
-    m_filterTimer.setInterval(500);
-    connect(ui->filterEdit, &QLineEdit::textChanged, [this]{m_filterTimer.start();});
-    connect(&m_filterTimer, &QTimer::timeout, [this]{
-        setFilter(ui->filterEdit->text());
-    });
-    connect(&m_model, &QAbstractItemModel::modelReset, this, &DataViewer::updateNavigation);
-    // ui->dataView->setItemDelegateForColumn(10, new BooleanDelegate(this));
-    QTimer::singleShot(0, this, &DataViewer::updateNavigation);
+  ui->setupUi(this);
+  ui->dataView->setModel(&m_model);
+  connect(&m_model, &QAbstractItemModel::rowsInserted, this, &DataViewer::updateNavigation);
+  connect(&m_model, &QAbstractItemModel::rowsRemoved, this, &DataViewer::updateNavigation);
+  connect(ui->firstButton, &QPushButton::clicked, [=]
+          {m_model.setPage(1); refresh(); });
+  connect(ui->nextButton, &QPushButton::clicked, [=]
+          {m_model.setPage(m_model.currentPage() + 1); refresh(); });
+  connect(ui->prevButton, &QPushButton::clicked, [=]
+          {m_model.setPage(m_model.currentPage() - 1); refresh(); });
+  connect(ui->lastButton, &QPushButton::clicked, [=]
+          {m_model.setPage(m_model.totalPages()); refresh(); });
+  m_filterTimer.setSingleShot(true);
+  m_filterTimer.setInterval(500);
+  connect(ui->filterEdit, &QLineEdit::textChanged, [this]
+          { m_filterTimer.start(); });
+  connect(&m_filterTimer, &QTimer::timeout, [this]
+          { setFilter(ui->filterEdit->text()); });
+  connect(&m_model, &QAbstractItemModel::modelReset, this, &DataViewer::updateNavigation);
+  // ui->dataView->setItemDelegateForColumn(10, new BooleanDelegate(this));
+  QTimer::singleShot(0, this, &DataViewer::updateNavigation);
+  auto *refreshShortcut = new QShortcut(QKeySequence::Refresh, this, nullptr, nullptr, Qt::WidgetWithChildrenShortcut);
+  connect(refreshShortcut, &QShortcut::activated, this, &DataViewer::refresh);
 }
 
-void DataViewer::setFilterColumnNames(const QStringList& sl) {
+void DataViewer::setFilterColumnNames(const QStringList &sl)
+{
   m_filterColumnNames.clear();
   m_filterColumnNames = sl;
 }
 
-
 DataViewer::~DataViewer()
 {
-    delete ui;
+  delete ui;
 }
 
 void DataViewer::setQueryArgs(const QString &query, const QVariantMap &bindings)
 {
-    m_model.setQueryArgs(query, bindings);
-    m_columns.clear();
-    auto m_record = m_model.record();
-    for(int i=0; i < m_record.count(); ++i) {
-      m_columns.insert(i, m_record.fieldName(i));
-    }
+  m_model.setQueryArgs(query, bindings);
+  m_columns.clear();
+  auto m_record = m_model.record();
+  for (int i = 0; i < m_record.count(); ++i)
+  {
+    m_columns.insert(i, m_record.fieldName(i));
+  }
 }
 
 void DataViewer::updateNavigation()
 {
-    if (m_model.totalPages() < 2)
-    {
-        ui->navigationFrame->hide();
-        return;
-    }
-    ui->navigationFrame->show();
-    ui->firstButton->setEnabled(m_model.currentPage() != 1);
-    ui->prevButton->setEnabled(m_model.currentPage() > 1);
-    ui->nextButton->setEnabled(m_model.currentPage() < m_model.totalPages());
-    ui->lastButton->setEnabled(m_model.currentPage() != m_model.totalPages());
-    ui->currentPage->setValue(m_model.currentPage());
-    ui->labelTotal->setText(QString("/%2").arg(m_model.totalPages()));
+  if (m_model.totalPages() < 2)
+  {
+    ui->navigationFrame->hide();
+    return;
+  }
+  ui->navigationFrame->show();
+  ui->firstButton->setEnabled(m_model.currentPage() != 1);
+  ui->prevButton->setEnabled(m_model.currentPage() > 1);
+  ui->nextButton->setEnabled(m_model.currentPage() < m_model.totalPages());
+  ui->lastButton->setEnabled(m_model.currentPage() != m_model.totalPages());
+  ui->currentPage->setValue(m_model.currentPage());
+  ui->labelTotal->setText(QString("/%2").arg(m_model.totalPages()));
 }
 
 void DataViewer::refresh()
 {
-    m_model.refresh();
-    updateNavigation();
-    emit refreshed();
+  m_model.refresh();
+  updateNavigation();
+  emit refreshed();
+  qDebug() << objectName() << "refreshed";
 }
 
 void DataViewer::setFilter(const QString &filter)
 {
-  if (m_filterColumnNames.isEmpty()) return ;
+  if (m_filterColumnNames.isEmpty())
+    return;
   QStringList filters;
-  for(auto const& fname : m_filterColumnNames) {
+  for (auto const &fname : m_filterColumnNames)
+  {
     filters << QString(" %1 LIKE '%%2%' ").arg(fname, filter);
   }
   m_model.setFilter(filters.join("OR"));
   updateNavigation();
 }
 
-void DataViewer::setColumnVisible(const QString& name, bool vis) {
-  for(auto const &[kk, kv] : m_columns.asKeyValueRange()) {
-    if ( kv == name ) {
+void DataViewer::setColumnVisible(const QString &name, bool vis)
+{
+  for (auto const &[kk, kv] : m_columns.asKeyValueRange())
+  {
+    if (kv == name)
+    {
       setColumnVisible(kk, vis);
-      return ;
+      return;
     }
   }
 }
 
-void DataViewer::setColumnVisible(int col, bool vis) {
-  if (col >= 0 && col < model().columnCount()) {
-    if(vis)
+void DataViewer::setColumnVisible(int col, bool vis)
+{
+  if (col >= 0 && col < model().columnCount())
+  {
+    if (vis)
       ui->dataView->showColumn(col);
     else
       ui->dataView->hideColumn(col);
   }
 }
 
-void DataViewer::setEditable(bool editable) {
+void DataViewer::setEditable(bool editable)
+{
   ui->dataView->setEditTriggers(editable ? QTableView::AllEditTriggers : QTableView::NoEditTriggers);
 }
 
